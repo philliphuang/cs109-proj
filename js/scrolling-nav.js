a9154a27730d4dbd6a869e40154ca738f5d5a02b
Christopher Chen <chris.chen796@gmail.com>
2016-12-15 00:37:50 -0500

    debugging website intro

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -19,19 +19,19 @@ $(function() {
 });
 
  
-// $('nav').hide();
-// $('section:not(#intro)').hide();
-// $('.subtitle').hide()
-// $('.title1').hide()
-// $('.title2').hide()
-// $('.title3').hide()
-// $('.title4').hide()
-// $('.title5').hide()
-// $('.title1').delay(2500).fadeIn(300)
-// $('.title2').delay(2900).fadeIn(300)
-// $('.title3').delay(3300).fadeIn(300)
-// $('.title4').delay(3700).fadeIn(300)
-// $('.title5').delay(4100).fadeIn(300)
-// $('.subtitle').delay(4500).fadeIn(600)
-// $('nav').delay(4500).fadeIn(600);
-// $('section:not(#intro)').delay(4500).fadeIn(600);
+$('nav').hide();
+$('section:not(#intro)').hide();
+$('.subtitle').hide()
+$('.title1').hide()
+$('.title2').hide()
+$('.title3').hide()
+$('.title4').hide()
+$('.title5').hide()
+$('.title1').delay(2500).fadeIn(300)
+$('.title2').delay(2900).fadeIn(300)
+$('.title3').delay(3300).fadeIn(300)
+$('.title4').delay(3700).fadeIn(300)
+$('.title5').delay(4100).fadeIn(300)
+$('.subtitle').delay(4500).fadeIn(600)
+$('nav').delay(4500).fadeIn(600);
+$('section:not(#intro)').delay(4500).fadeIn(600);
